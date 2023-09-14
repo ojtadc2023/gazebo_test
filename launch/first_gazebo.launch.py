a6dd05db8d37e1781890bb50b68d935dd3e63c5e
ojtPC <ojt.adc.2023@gmail.com>
2023-09-14 11:48:41 +0900

    gazebo tutorial

---
+++ launch/first_gazebo.launch.py
@@ -0,0 +1,42 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+from launch.substitutions import LaunchConfiguration
+
+def generate_launch_description():
+
+    package_dir = get_package_share_directory("gazebo_test")
+    urdf = os.path.join(package_dir, "urdf" , "rover.urdf")
+    # rviz = os.path.join(package_dir, "rviz" , "rover.rviz")
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+
+    return LaunchDescription([
+
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            arguments=[urdf],
+            ),
+            
+        Node(
+            package="rviz2",
+            executable="rviz2",
+            name="rviz2",
+            # arguments=["-d", rviz]
+            ),
+        
+        # gazebo settings
+        ExecuteProcess(
+            cmd=["gazebo", "--verbose", "-s", "libgazebo_ros_factory.so"],
+            ),
+
+        Node(
+            package="gazebo_ros",
+            executable="spawn_entity.py",
+            name="urdf_spawner",
+            parameters=[{'use_sim_time': use_sim_time}],
+            arguments=["-topic", "/robot_description", "-entity", "rover"]),
+    ])
